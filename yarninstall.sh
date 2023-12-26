a1a5f63c2fc5bf9de59a4665c147be17d7a26c5e
tomv <personnal@tomv.ovh>
2023-12-27 00:24:14 +0100

    Update | yarninstall.sh - Remove 'sudo'
    J'ai enlevé les commandes en préfix sudo.

--- yarninstall.sh
+++ yarninstall.sh
@@ -33,8 +33,8 @@ echo ""
 
 sleep 1
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 ##
 
 ## INSTALLATION DOCKER | Last Version ##
