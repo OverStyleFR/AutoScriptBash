cb0c91cf9860e41bd3828f1d918ed3c60f1325b8
tomv <personnal@tomv.ovh>
2023-12-27 19:13:35 +0100

    Update | Initialisation unzip

--- .assets/initialisation.sh
+++ .assets/initialisation.sh
@@ -18,3 +18,37 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Le reste du script ici
+
+########################################### INITIALISATION / PREPARATION ################################
+
+################ PACKAGES #################
+
+### UNZIP ###
+
+# Vérifier si le package unzip est installé
+if ! dpkg -s unzip >/dev/null 2>&1; then
+    # Afficher le texte en rouge et en gras
+    echo "${BOLD}Le package unzip n'est pas installé. Installation en cours...${RESET}"
+    sudo apt-get update >/dev/null 2>&1
+    sudo apt-get install -y unzip >/dev/null 2>&1
+else
+    # Afficher le texte en vert et en gras
+    echo "${BOLD}Le package unzip est déjà installé.${RESET}"
+fi
+
+### YARN ### (Ré-installation de celui ci)
+
+### PHP ###
+
+
+
+
+
+
+
+
+
+################ VERSIONS #################
+
+### Node JS ### (16.20.2)
+
