ac2c5bc0f8c6c73295c50a120cca7e9b9fdf11bb
tomv <personnal@tomv.ovh>
2023-12-27 18:11:16 +0100

    Update | Add menu to main file

--- pterodactylthemeinstaller.sh
+++ pterodactylthemeinstaller.sh
@@ -19,3 +19,37 @@ fi
 
 # Le reste du script ici
 
+while true; do
+    # Affichage du menu
+    echo "Menu:"
+    echo "1. Installer Stellar"
+    echo "2. Installer Enigma"
+    echo "3. Re-installer le thème du panel (RESET)"
+    echo "4. Quitter"
+
+    # Lecture du choix de l'utilisateur
+    read -p "Choisissez une option (1-4) : " choix
+
+    # Traitement du choix
+    case $choix in
+        1)
+            echo "Vous avez de installer le thème Stellar."
+            # Ajoutez le code correspondant à l'Option 1 ici
+            ;;
+        2)
+            echo "Vous avez de installer le thème Enigma."
+            # Ajoutez le code correspondant à l'Option 2 ici
+            ;;
+        3)
+            echo "Vous avez de ré-installer le thème de Pterodactyl. (RESET)"
+            # Ajoutez le code correspondant à l'Option 3 ici
+            ;;
+        4)
+            echo "Au revoir !"
+            exit 0
+            ;;
+        *)
+            echo "Choix non valide. Veuillez entrer un numéro entre 1 et 4."
+            ;;
+    esac
+done
