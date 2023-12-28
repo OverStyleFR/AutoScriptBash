ab497d0b803d96b81f798ec4f86d9b7d1156013f
tomv <personnal@tomv.ovh>
2023-12-28 01:31:29 +0100

    Update | Stellar - Communication

--- .assets/theme_stellar.sh
+++ .assets/theme_stellar.sh
@@ -28,16 +28,22 @@ dossier="/tmp/pterodactylthemeinstaller"
 if [ -d "$dossier" ]; then
     # Vérifier si le dossier est vide
     if [ -z "$(ls -A $dossier)" ]; then
-        echo "Le dossier existe mais est vide."
+        echo ""
+        echo "${BOLD}Le dossier existe mais est vide.${RESET}"
+        echo ""
     else
         # Supprimer le contenu du dossier s'il n'est pas vide
         rm -r "$dossier"/*
-        echo "Le contenu du dossier a été supprimé avec succès."
+        echo ""
+        echo "${RED}${BOLD}Le contenu du dossier a été supprimé avec succès.${RESET}"
+        echo ""
     fi
 else
     # Créer le dossier s'il n'existe pas
     mkdir -p "$dossier"
-    echo "Le dossier a été créé avec succès."
+    echo ""
+    echo "${GREEN}${BOLD}Le dossier a été créé avec succès.${RESET}"
+    echo ""
 fi
 
 # Fonction pour le choix 1
@@ -46,19 +52,28 @@ choice_one() {
     ### DOWNLOAD ###
 
     cd /tmp/pterodactylthemeinstaller
+    echo ""
+    echo "${BOLD}Téléchargement du thème :${RESET}"
+    echo ""
     wget -O stellar-v3.3.zip https://files.catbox.moe/ldjbsz.zip
 
     # Vérifier si le téléchargement a réussi
     if [ -f "stellar-v3.3.zip" ]; then
         # Extraire les dossiers spécifiques du ZIP
         cd /tmp/pterodactylthemeinstaller
-        unzip stellar-v3.3.zip
+        echo ""
+        echo "${BOLD}Extraction du thème...${RESET}"
+        unzip stellar-v3.3.zip > /dev/null 2>&1
+        echo "${BOLD}Déplacement du thème...${RESET}"
         rsync -a --remove-source-files pterodactyl/app pterodactyl/resources pterodactyl/database pterodactyl/routes /var/www/pterodactyl
+        echo ""
 
         # Supprimer le fichier ZIP après l'extraction (si nécessaire)
+        echo "${BOLD}Suppresion du fichier temporaire...${RESET}"
         rm stellar-v3.3.zip
+        echo ""
     else
-        echo "Échec du téléchargement du fichier ZIP."
+        echo "${RED}${BOLD}Échec du téléchargement du fichier ZIP.${RESET}"
         exit 1
     fi
 
@@ -66,19 +81,28 @@ choice_one() {
     cd /var/www/pterodactyl/ || exit
 
     # Installer react-feather via Yarn
-    yarn add react-feather
+    echo "${BOLD}Installation de 'react-feather' via yarn...${RESET}"
+    yarn add react-feather > /dev/null 2>&1
 
     ## NPX Installation
-    npx update-browserslist-db@latest
+    echo "${BOLD}Mise à jour de NPX...${RESET}"
+    npx update-browserslist-db@latest > /dev/null 2>&1
 
     # Exécuter les migrations
-    php artisan migrate <<< "yes"
+    echo "${BOLD}Migration en cour...${RESET}"
+    php artisan migrate <<< "yes" > /dev/null 2>&1
 
     # Construire la version de production
-    yarn build:production
+    echo ""
+    echo "${VIOLET}${BOLD}Re-build du thème en cour...${RESET}"
+    echo ""
+    yarn build:production > /dev/null 2>&1
+    echo "${GREEN}${BOLD}Build Terminé !.${RESET}"
 
     # Effacer le cache des vues
-    php artisan view:clear
+    echo ""
+    echo "${BOLD}Effacement du cache.${RESET}"
+    php artisan view:clear > /dev/null 2>&1
 }
 
 # Fonction pour le choix 2
@@ -87,10 +111,16 @@ choice_two() {
     cd /var/www/pterodactyl/ || exit
 
     # Construire la version de production
-    yarn build:production
+        echo ""
+    echo "${VIOLET}${BOLD}Re-build du thème en cour...${RESET}"
+    echo ""
+    yarn build:production > /dev/null 2>&1
+    echo "${GREEN}${BOLD}Build Terminé !.${RESET}"
 
     # Effacer le cache des vues
-    php artisan view:clear
+    echo ""
+    echo "${BOLD}Effacement du cache.${RESET}"
+    php artisan view:clear > /dev/null 2>&1
 }
 
 # Affichage du menu de choix
