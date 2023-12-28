c43cba32f9d42eb8957e225afd29592467460d13
tomv <personnal@tomv.ovh>
2023-12-28 15:34:39 +0100

    Update | Ptero Panel Re-install - Temp Folder

--- pterodactylpanelreinstall.sh
+++ pterodactylpanelreinstall.sh
@@ -25,7 +25,34 @@ echo ""
 echo "${GREEN}${BOLD}Auto-Save .env File${RESET}"
 echo ""
 
-mkdir /tmp/pterodactylpanelreinstall
+### DOSSIER TEMPORAIRE ###
+
+# Définir le chemin du dossier à vérifier
+dossier="/tmp/pterodactylpanelreinstall"
+
+# Vérifier si le dossier existe
+if [ -d "$dossier" ]; then
+    # Vérifier si le dossier est vide
+    if [ -z "$(ls -A $dossier)" ]; then
+        echo ""
+        echo "${BOLD}Le dossier existe mais est vide.${RESET}"
+        echo ""
+    else
+        # Supprimer le contenu du dossier s'il n'est pas vide
+        rm -r "$dossier"/*
+        echo ""
+        echo "${RED}${BOLD}Le contenu du dossier a été supprimé avec succès.${RESET}"
+        echo ""
+    fi
+else
+    # Créer le dossier s'il n'existe pas
+    mkdir -p "$dossier"
+    echo ""
+    echo "${GREEN}${BOLD}Le dossier a été créé avec succès.${RESET}"
+    echo ""
+fi
+
+
 mv /var/www/pterodactyl/.env /tmp/pterodactylpanelreinstall/
 
 
